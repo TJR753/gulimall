0887986d788059faafcc956ae57f04e5044ae5c8
tjr <tjr1819324794>
2022-02-07 19:00:13 +0800

    完成oss上传

--- gulimall-common/src/test/java/com/example/gulimall/common/GulimallCommonApplicationTests.java
+++ gulimall-common/src/test/java/com/example/gulimall/common/GulimallCommonApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.gulimall.common;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootTest
 class GulimallCommonApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
